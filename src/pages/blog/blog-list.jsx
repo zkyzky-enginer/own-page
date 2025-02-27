b48646f2c193ce9f63984a3fa193a261ba6af165
zkyzky-enginer <1161437723@qq.com>
2025-02-27 18:30:49 +0800

    初始化技术博客页面框架

--- src/pages/blog/blog-list.jsx
+++ src/pages/blog/blog-list.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from 'antd';
+import { Card, Flex } from 'antd';
 
 // export function BlogCard() {
 // 	return <></>;
@@ -8,11 +8,15 @@ import { Card } from 'antd';
 export default function BlogList() {
 	return (
 		<>
-			<Card title="Card title" variant="borderless" style={{ width: 300 }}>
-				<p>Card content</p>
-				<p>Card content</p>
-				<p>Card content</p>
-			</Card>
+			<Flex wrap gap="large">
+				{Array.from({ length: 10 }).map((_, index) => (
+					<Card className="inline-block" key={index} title={`Card title ${index + 1}`} variant="borderless" style={{ width: 300 }}>
+						<p>Card content</p>
+						<p>Card content</p>
+						<p>Card content</p>
+					</Card>
+				))}
+			</Flex>
 		</>
 	);
 }
